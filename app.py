ece04df8d8e26b711f226ddbdc47463d82f7c3d4
Nikolay Andreev <bakasaru@list.ru>
2021-02-06 20:05:58 +0300

    Main page with Flask

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from games import games
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def homepage():
+    return render_template('index.html', title="Nintendon't", games=games)
+
+
+@app.route('/about')
+def about():
+    return 'About us'
+
+
+@app.route('/games/<int:game_id>')
+def get_game(game_id):
+    return f'Game #{game_id}'
 
 
 if __name__ == '__main__':
---
+++ games.py
@@ -0,0 +1,23 @@
+games = {
+    1: {
+        'name': 'Super Mario 3D World + Bowser\'s Fury',
+        'price': 4499
+    },
+    2: {
+        'name': 'Super Mario Odyssey',
+        'price': 4499
+    },
+    3: {
+        'name': 'The Legend Of Zelda: Breath of the Wild',
+        'price': 5249
+    },
+    4: {
+        'name': 'Astral Chain',
+        'price': 4499
+    },
+    5: {
+        'name': 'NBA 2K20',
+        'price': 179,
+        'sale': True
+    },
+}
